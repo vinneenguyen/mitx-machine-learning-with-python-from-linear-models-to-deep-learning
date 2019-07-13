8931066b81160790418617a0fceb29428243ae7c
Vinh Xuan Nguyen <32093774+nxv1987@users.noreply.github.com>
2019-07-13 17:07:36 +1000

    Update main.py

--- Project 2: Digit recognition (Part 1)/mnist/part1/main.py
+++ Project 2: Digit recognition (Part 1)/mnist/part1/main.py
@@ -40,7 +40,9 @@ def run_linear_regression_on_MNIST(lambda_factor=1):
 
 
 # Don't run this until the relevant functions in linear_regression.py have been fully implemented.
-print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=1))
+print('Linear Regression test_error (lambda=1) =', run_linear_regression_on_MNIST(lambda_factor=1))
+print('Linear Regression test_error (lambda=0.1) =', run_linear_regression_on_MNIST(lambda_factor=0.1))
+print('Linear Regression test_error (lambda=0.01) =', run_linear_regression_on_MNIST(lambda_factor=0.01))
 
 
 #######################################################################
