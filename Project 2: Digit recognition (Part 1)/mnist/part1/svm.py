0e479d37d9bccfdd06298eaa3028597b3043d73a
Vinh Xuan Nguyen <32093774+nxv1987@users.noreply.github.com>
2019-07-13 18:36:14 +1000

    Update svm.py

--- Project 2: Digit recognition (Part 1)/mnist/part1/svm.py
+++ Project 2: Digit recognition (Part 1)/mnist/part1/svm.py
@@ -32,7 +32,11 @@ def multi_class_svm(train_x, train_y, test_x):
     Returns:
         pred_test_y - (m,) NumPy array containing the labels (int) for each test data point
     """
-#     #raise NotImplementedError
+    clf = LinearSVC(random_state=0, C=0.1)
+    clf.fit(train_x, train_y)
+    preds = clf.predict(test_x)
+    
+    return preds
 
 
 def compute_test_error_svm(test_y, pred_test_y):
