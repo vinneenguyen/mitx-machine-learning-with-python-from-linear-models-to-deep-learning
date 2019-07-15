3ffd710ac0ffca6a59946582c854cf8f9183cf47
Vinh Xuan Nguyen <32093774+nxv1987@users.noreply.github.com>
2019-07-15 17:38:08 +1000

    Update test.py

--- Project 2: Digit recognition (Part 1)/mnist/part1/test.py
+++ Project 2: Digit recognition (Part 1)/mnist/part1/test.py
@@ -235,9 +235,10 @@ def check_project_onto_PC():
     ]);
     pcs = features.principal_components(X)
     exp_res = np.array([
-        [2, 0, 0],
-        [0, 0, 0],
-        [-2, 0, 0]
+        [5.61248608, 0],
+        [1.87082869, 0],
+        [-1.87082869, 0],
+        [-5.61248608, 0],
     ])
     n_components = 3
     if check_array(
